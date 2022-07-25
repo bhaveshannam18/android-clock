e4ae725f2270b89335d47c989df07f5aa935f132
Bhavesh Annam <bhaveshannam18@gmail.com>
2022-07-25 22:34:55 +0530

    Added Current Time

--- src/Clock/WorldClock/WorldClock.js
+++ src/Clock/WorldClock/WorldClock.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+import "./WorldClock.css";
+
 function WorldClock(){
+    const [currentTime,setCurrentTime] = useState("...loading");
+
+    let addZeroToTime = (digit)=>{
+        if(digit < 10){
+            digit = "0" + digit;
+        }
+        return digit;
+    }
+
+    setInterval(()=>{
+        var timeObj = new Date();
+        let hours = addZeroToTime((timeObj.getHours())%12);
+        let mins = addZeroToTime(timeObj.getMinutes());
+        let secs = addZeroToTime(timeObj.getSeconds());
+        let finalTime = hours + " : " + mins + " : " + secs;
+        setCurrentTime(finalTime);
+    },1000);
+
     return (<>
-        <h1>This is WorldClock State</h1>
+        <div className="worldClockContainer">
+            <h1>Current Time</h1>
+            <h1>{currentTime}</h1>
+        </div>
     </>);
 }
 
